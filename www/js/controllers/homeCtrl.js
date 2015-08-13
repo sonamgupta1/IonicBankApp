f7d5a74c08a525045986c1ecdc538d3ef2675ae3
sonam <sonam@headerlabs.com>
2015-08-13 12:13:26 +0530

    ionic blank app with home page

---
+++ www/js/controllers/homeCtrl.js
@@ -0,0 +1,9 @@
+/**
+ * Created by sonam on 13/8/15.
+ */
+
+IonicBankApp.controller('HomeCtrl',function($scope,$state){
+
+
+
+})
---
+++ www/js/services/home.js
@@ -0,0 +1,7 @@
+/**
+ * Created by sonam on 13/8/15.
+ */
+IonicBankApp.factory('HomeService', function ($location) {
+
+
+})
